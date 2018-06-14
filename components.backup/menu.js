ce79ec6b99df11e97177d39505fd3b3dcc36d86e
Alejandro Velazquez <hi@alein.uk>
2018-06-14 09:56:04 +0100

    html indent

---
+++ components.backup/menu.js
@@ -0,0 +1,9 @@
+
+function on() {
+    document.getElementById("mobile-menu").style.display = "block";
+    document.querySelector('body').style.overflow = 'hidden';
+}
+function off() {
+    document.getElementById("mobile-menu").style.display = "none";
+    document.querySelector('body').style.overflow = '';
+}
