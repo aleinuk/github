37c290dec7c7082954bfc51f5b34cbe407b80268
Alejandro Velazquez <hi@alein.uk>
2018-07-20 11:20:35 +0100

    fluid

---
+++ fluid/menu.js
@@ -0,0 +1,10 @@
+
+function on() {
+    document.getElementById("mobile-menu").style.display = "block";
+    document.querySelector('body').style.overflow = 'hidden';
+}
+function off() {
+    document.getElementById("mobile-menu").style.display = "none";
+    document.querySelector('body').style.overflow = '';
+
+}
